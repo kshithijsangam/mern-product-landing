5baa5c2726f1ce659a43f82732df6c628b5437b8
Kshithij Sangam <kshithijsangam200217@gmail.com>
2025-11-23 11:04:22 +0530

    Frontend using Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,5 +4,5 @@ const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://mern-product-landing.onrender.com",
 });
